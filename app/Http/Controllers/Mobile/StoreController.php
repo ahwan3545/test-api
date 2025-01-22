7b0ff3f05ddd94fdca6139f440addd4a15f9ede9
ahwan3545 <ahwanahwan.3545@gmail.com>
2025-01-22 19:52:36 +0300

    Change store api ti static

--- app/Http/Controllers/Mobile/StoreController.php
+++ app/Http/Controllers/Mobile/StoreController.php
@@ -13,7 +13,30 @@ class StoreController extends Controller
 
     public function index(Request $request)
     {
-        $stores = Store::select(['id', 'name', 'image'])->get();
+        // $stores = Store::select(['id', 'name', 'image'])->get();
+
+        $stores = [
+            [
+                "id" => 1,
+                "name" => "Amazon",
+                "image" => "https://test-api-production-4d83.up.railway.app/assets/images/stores/amazon.png"
+            ],
+            [
+                "id" => 2,
+                "name" => "eBay",
+                "image" => "https://test-api-production-4d83.up.railway.app/assets/images/stores/eBay.png"
+            ],
+            [
+                "id" => 3,
+                "name" => "Walmart",
+                "image" => "https://test-api-production-4d83.up.railway.app/assets/images/stores/walmart.png"
+            ],
+            [
+                "id" => 4,
+                "name" => "AliExpress",
+                "image" => "https://test-api-production-4d83.up.railway.app/assets/images/stores/aliexpress.png"
+            ]
+        ];
 
         return $this->success($stores, 'Stores retrieved successfully.');
     }
